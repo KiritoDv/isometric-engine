ee34727b82b96e6c7967cdfa7a513eee7069b03e
Alexis <kiritodev01@gmail.com>
2019-10-13 23:23:17 -0500

    Fixed grid system

--- main.js
+++ main.js
@@ -60,23 +60,6 @@ function draw() {
 	camera.on();
 	//camera(player.x, player.y, (height / 2) / tan(PI / 6), 0, 0, 0, 0, 1, 0);		
 	
-	map.points.forEach((a, i) => {
-		if(a.d && a.tile.texture != null){
-			//var baseX = 0;			
-			//var baseY = 0;		
-			//var x = baseX + ((a.x - a.z) * (32 / 2)) - (32 / 2);
-			//var y = baseY + (((a.x + a.z) * (32 / 4)) - ((32 / 2) * (a.y))) - (32/ 2);
-			var x = baseX + Math.round(((a.x - a.y) * (a.tile.texture.width / 2)))
-			var y = baseY + Math.round(((a.x + a.y) * (a.tile.texture.height / 4)))
-			if(x > - 32 && x < windowWidth && y < windowHeight && y > -32){				
-				image(a.tile.texture, x, y, 32, 32)
-			}			
-			//textAlign(CENTER);
-			//textSize(8);			
-			//text(i, x+16, y, 12);
-		}
-	})
-
 	push()
 	stroke(0,0,0, 50)
 	translate(baseX+16, baseY)
@@ -87,6 +70,47 @@ function draw() {
 		line(x*16, x*8, (-((this.map.mapX)/2)*32)+(x*16), (((this.map.mapY)/2)*16)+(x*8));
 	}	
 	pop()
+
+	var mX = (mouseX - 16) - baseX
+	var mY = (mouseY - 8) - baseY
+
+	var xC = Math.round(((mX / (32 / 2) + mY / (32 / 4)) / 2));
+	var yC = Math.round(((mY / (32 / 4) - (mX / (32 / 2))) / 2));
+
+	//text("X: "+xC, mouseX+20, mouseY+10, 12);
+	//text("Y: "+yC, mouseX+60, mouseY+10, 12);	
+
+	var a = this.map.points.find(a => a.x == xC && a.y == yC && a.z == 0);	
+
+	var gX = Math.round(((xC - yC) * (32 / 2)))
+	var gY = Math.round(((xC + yC) * (32 / 4)))
+
+	if(xC >= 0 && xC < map.mapX && yC >= 0 && yC < map.mapY){
+		push()	
+		translate(baseX + gX, (baseY + gY)+8)
+		line(16, -8, 0, 0);
+		line(16, 8, 0, 0);
+		line(32, 0, 16, -8);
+		line(32, 0, 16, 8);
+		pop()
+	}
+
+	map.points.forEach((a, i) => {
+		if(a.d && a.tile.texture != null){
+			//var baseX = 0;			
+			//var baseY = 0;		
+			//var x = baseX + ((a.x - a.z) * (32 / 2)) - (32 / 2);
+			//var y = baseY + (((a.x + a.z) * (32 / 4)) - ((32 / 2) * (a.y))) - (32/ 2);
+			var x = baseX + Math.round(((a.x - a.y) * (a.tile.texture.width / 2)))
+			var y = baseY + Math.round((((a.x + a.y) * (a.tile.texture.height / 4)))-16)
+			if(x > - 32 && x < windowWidth && y < windowHeight && y > -32){				
+				image(a.tile.texture, x, y, 32, 32)
+			}			
+			//textAlign(CENTER);
+			//textSize(8);			
+			//text(i, x+16, y, 12);
+		}
+	})	
 
 	var ba = [0, 1, 2, 3, 4, 5, 6]
 	
@@ -132,46 +156,13 @@ function draw() {
 	//var xC = (camera.mouseX / 16 + camera.mouseX / 16) / 2;
 	//var yC = ((camera.mouseY*8) / 32 - (camera.mouseX / 8)) /2;
 
-	var xC = Math.floor((camera.mouseX + (16 / 2.0)) / 16) - baseX;
-    var yC = Math.floor(camera.mouseY / 16) - baseY;
-
-	var tP = {
-		x: yC + xC,
-		y: xC - yC -1
-	}
-
 	//var x = (camera.mouseX/16);
 	//var y = (camera.mouseY/16)
 	//var y = (((a.x + a.z) * (32 / 4)) - ((32 / 2) * (camera.mouseY))) - (32/ 2);	
 
 	//text("X: "+Math.round((map.mapX+x)/2), mouseX+20, mouseY+10, 12);
 	//text("Y: "+Math.round((map.mapZ+y)/24), mouseX+60, mouseY+10, 12);
-
-	camera.off()
-
-	var mX = (mouseX - 16) - baseX
-	var mY = (mouseY - 8) - baseY
-
-	var xC = Math.round(((mX / (32 / 2) + mY / (32 / 4)) / 2));
-	var yC = Math.round(((mY / (32 / 4) - (mX / (32 / 2))) / 2));
-
-	//text("X: "+xC, mouseX+20, mouseY+10, 12);
-	//text("Y: "+yC, mouseX+60, mouseY+10, 12);	
-
-	var a = this.map.points.find(a => a.x == xC && a.y == yC && a.z == 0);	
-
-	var gX = Math.round(((xC - yC) * (32 / 2)))
-	var gY = Math.round(((xC + yC) * (32 / 4)))
-
-	if(xC >= 0 && xC < map.mapX && yC >= 0 && yC < map.mapY){
-		push()	
-		translate(baseX + gX, (baseY + gY)+8)
-		line(16, -8, 0, 0);
-		line(16, 8, 0, 0);
-		line(32, 0, 16, -8);
-		line(32, 0, 16, 8);
-		pop()
-	}
+	
 	if(a && mouseIsPressed){				
 		if (mouseButton === LEFT && a.tile.texture) {
 			a.tile.texture = null;
